2e3a4ebdee0ab89e161095f2c2f0ec1f074c57ec
Victor Nazario <victor.nazario@upr.edu>
2020-03-14 02:00:14 -0400

    Water mock response

--- src/main/java/com/terremotospr/controllers/WaterController.java
+++ src/main/java/com/terremotospr/controllers/WaterController.java
@@ -1,12 +1,16 @@
 package com.terremotospr.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.terremotospr.beans.WaterBean;
 import com.terremotospr.services.WaterService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -23,7 +27,10 @@ public class WaterController {
     WaterService waterService;
 
     @GetMapping(value = "/available")
-    public List<WaterBean> fetchAvailableWater(){
-        return waterService.fetchAllWater();
+    public Object fetchAvailableWater() throws IOException {
+        //To obtain the path, in IDEA rightclick and when the dialog shows up, select copy path -> path from source root
+        Resource resource = new ClassPathResource("responses/waterResponseJSON.json");
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(resource.getInputStream(), Object.class);
     }
 }
