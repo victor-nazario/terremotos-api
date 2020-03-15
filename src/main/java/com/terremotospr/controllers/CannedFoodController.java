cc2a8175f9d8301d90415804dbb25cf75b7e83b5
Kiara Rodriguez Rojas <kiara.rodriguez18@upr.edu>
2020-03-15 15:14:27 -0500

    Finished Canned Foods Entity

--- src/main/java/com/terremotospr/controllers/CannedFoodController.java
+++ src/main/java/com/terremotospr/controllers/CannedFoodController.java
@@ -1,8 +1,8 @@
 package com.terremotospr.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.terremotospr.beans.IceBean;
-import com.terremotospr.services.IceService;
+import com.terremotospr.beans.CannedFoodBean;
+import com.terremotospr.services.CannedFoodService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
@@ -16,26 +16,26 @@ import java.io.IOException;
  */
 
 @RestController
-@RequestMapping(value = "/ice")
-public class IceController {
+@RequestMapping(value = "/cannedfood")
+public class CannedFoodController {
 
     @Autowired
-    IceService iceService;
+    CannedFoodService cannedFoodService;
 
     @PostMapping(value = "/add")
-    public boolean add(@RequestBody IceBean bean) {
-        return iceService.addIce(bean);
+    public boolean add(@RequestBody CannedFoodBean bean) {
+        return cannedFoodService.addCannedFood(bean);
     }
 
 //    @GetMapping(value = "/fetch")
-//    public List<IceBean> fetchAll(){
-//        return iceService.fetchAllIce();
+//    public List<CannedFoodBean> fetchAll(){
+//        return cannedFoodService.fetchAllCannedFood();
 //    }
-
+//
     @GetMapping(value = "/fetch")
     public Object fetchAll() throws IOException {
         //To obtain the path, in IDEA rightclick and when the dialog shows up, select copy path -> path from source root
-        Resource resource = new ClassPathResource("responses/iceResponseJSON.json");
+        Resource resource = new ClassPathResource("responses/cannedFoodResponseJSON.json");
         ObjectMapper mapper = new ObjectMapper();
         return mapper.readValue(resource.getInputStream(), Object.class);
     }
