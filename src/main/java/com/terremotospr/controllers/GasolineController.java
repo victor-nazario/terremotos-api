d55a6d80b9a98130f94b6512655ffb300a00190b
Kiara Rodriguez Rojas <kiara.rodriguez18@upr.edu>
2020-03-14 23:08:18 -0500

    Changed GasolineController

--- src/main/java/com/terremotospr/controllers/GasolineController.java
+++ src/main/java/com/terremotospr/controllers/GasolineController.java
@@ -4,6 +4,7 @@ import com.terremotospr.beans.GasolineBean;
 import com.terremotospr.services.GasolineService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +21,7 @@ public class GasolineController {
     GasolineService gasolineService;
 
     @PostMapping(value = "/add")
-    public boolean add(GasolineBean bean){
+    public boolean add(@RequestBody GasolineBean bean){
         return gasolineService.addGasoline(bean);
     }
 }
