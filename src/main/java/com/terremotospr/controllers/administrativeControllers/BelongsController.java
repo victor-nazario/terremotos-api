3a31d1c81bb6e3730a9c6b7917a3dcf077fa8e5b
fred.aponte <wilfredo.aponte@upr.edu>
2020-03-30 15:58:24 -0400

    Json Responses Updated

--- src/main/java/com/terremotospr/controllers/administrativeControllers/BelongsController.java
+++ src/main/java/com/terremotospr/controllers/administrativeControllers/BelongsController.java
@@ -1,10 +1,14 @@
 package com.terremotospr.controllers.administrativeControllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.terremotospr.beans.administrativeBeans.BelongsBean;
 import com.terremotospr.services.administrativeServices.BelongsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -18,16 +22,16 @@ public class BelongsController {
         return belongsService.addBelongs(bean);
     }
 
-    @GetMapping(value = "/fetch")
-    public List<BelongsBean> fetchAll(){
-        return belongsService.fetchAllBelongs();
-    }
-
 //    @GetMapping(value = "/fetch")
-//    public Object fetchAll() throws IOException {
-//        //To obtain the path, in IDEA right click and when the dialog shows up, select copy path -> path from source root
-//        Resource resource = new ClassPathResource("responses/suppliesResponseJSON.json");
-//        ObjectMapper mapper = new ObjectMapper();
-//        return mapper.readValue(resource.getInputStream(), Object.class);
+//    public List<BelongsBean> fetchAll(){
+//        return belongsService.fetchAllBelongs();
 //    }
+
+    @GetMapping(value = "/fetch")
+    public Object fetchAll() throws IOException {
+        //To obtain the path, in IDEA right click and when the dialog shows up, select copy path -> path from source root
+        Resource resource = new ClassPathResource("responses/belongsResponseJSON.json");
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(resource.getInputStream(), Object.class);
+    }
 }
