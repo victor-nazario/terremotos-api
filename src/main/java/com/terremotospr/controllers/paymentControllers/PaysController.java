64724153b471b430abb09dd4ea037477336b7e67
fred.aponte <wilfredo.aponte@upr.edu>
2020-03-31 14:08:03 -0400

    Fetch from json response

--- src/main/java/com/terremotospr/controllers/paymentControllers/PaysController.java
+++ src/main/java/com/terremotospr/controllers/paymentControllers/PaysController.java
@@ -1,10 +1,14 @@
 package com.terremotospr.controllers.paymentControllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.terremotospr.beans.paymentBeans.PaysBean;
 import com.terremotospr.services.paymentServices.PaysService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -22,15 +26,16 @@ public class PaysController {
         return paysService.addPays(bean);
     }
 
-    @GetMapping(value = "/fetch")
-    public List<PaysBean> fetchAll(){
-        return paysService.fetchAllPays();
-    }
-
 //    @GetMapping(value = "/fetch")
-//    public Object fetchAll() throws IOException {
-//        //To obtain the path, in IDEA right click and when the dialog shows up, select copy path -> path from source root
-//        Resource resource = new ClassPathResource("responses/paysResponseJSON.json");
-//        ObjectMapper mapper = new ObjectMapper();
-//        return mapper.readValue(resource.getInputStream(), Object.class);
+//    public List<PaysBean> fetchAll(){
+//        return paysService.fetchAllPays();
+//    }
+
+    @GetMapping(value = "/fetch")
+    public Object fetchAll() throws IOException {
+        //To obtain the path, in IDEA right click and when the dialog shows up, select copy path -> path from source root
+        Resource resource = new ClassPathResource("responses/paysResponseJSON.json");
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(resource.getInputStream(), Object.class);
+    }
 }
