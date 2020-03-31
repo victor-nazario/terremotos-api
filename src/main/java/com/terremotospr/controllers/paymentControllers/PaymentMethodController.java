a79423339be022995316e57b062494480a8f950a
Victor Nazario <victor.nazario@upr.edu>
2020-03-31 19:39:39 -0400

    Payment method controller fixes

--- src/main/java/com/terremotospr/controllers/paymentControllers/PaymentMethodController.java
+++ src/main/java/com/terremotospr/controllers/paymentControllers/PaymentMethodController.java
@@ -1,12 +1,14 @@
 package com.terremotospr.controllers.paymentControllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.terremotospr.beans.paymentBeans.PaymentMethodBean;
 import com.terremotospr.services.paymentServices.PaymentMethodService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
-import java.util.List;
 
 /**
  * @author Kiara Rodriguez Rojas
@@ -20,12 +22,11 @@ public class PaymentMethodController {
     PaymentMethodService paymentMethodService;
 
     @GetMapping(value = "/fetch")
-    public List<PaymentMethodBean> fetchAllPaymentMethod() throws IOException {
+    public Object fetchAllPaymentMethod() throws IOException {
         //To obtain the path, in IDEA rightclick and when the dialog shows up, select copy path -> path from source root
-//        Resource resource = new ClassPathResource("paymentMethodResponseJSON.json");
-//        ObjectMapper mapper = new ObjectMapper();
-//        return mapper.readValue(resource.getInputStream(), Object.class);
-        return paymentMethodService.fetchAllPaymentMethod();
+        Resource resource = new ClassPathResource("paymentMethodResponseJSON.json");
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(resource.getInputStream(), Object.class);
     }
 
     @PostMapping(value = "/add")
