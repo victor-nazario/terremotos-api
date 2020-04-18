6f7109c60b313b37c230e2211f7e56b77148498c
Victor Nazario <victor.nazario@upr.edu>
2020-04-18 15:37:06 -0400

    Native query usage

--- src/main/java/com/terremotospr/controllers/administrativeControllers/AdminController.java
+++ src/main/java/com/terremotospr/controllers/administrativeControllers/AdminController.java
@@ -1,14 +1,11 @@
 package com.terremotospr.controllers.administrativeControllers;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.terremotospr.beans.administrativeBeans.AdminBean;
 import com.terremotospr.services.administrativeServices.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
+import java.util.List;
 
 /**
  * Created on March 17, 2020 - 1:02 AM
@@ -23,12 +20,8 @@ public class AdminController {
     AdminService adminService;
 
     @GetMapping(value = "/fetch")
-    public Object fetchAdmin() throws IOException {
-        //To obtain the path, in IDEA rightclick and when the dialog shows up, select copy path -> path from source root
-        Resource resource = new ClassPathResource("responses/adminResponseJSON.json");
-        ObjectMapper mapper = new ObjectMapper();
-        return mapper.readValue(resource.getInputStream(), Object.class);
-//        return adminService.fetchAllAdmin();
+    public List<AdminBean> fetchAdmin(){
+         return adminService.fetchAllAdmin();
     }
 
     @PostMapping(value = "/add")
