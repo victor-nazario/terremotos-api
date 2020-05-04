7de7311b016375492ae62dbf8e245d34c4f3f4e8
Kiara Rodríguez Rojas <kiara.rodriguez18@upr.edu>
2020-05-04 06:01:39 -0400

    Added Manages queries: fetchAll and findBy attributes

--- src/main/java/com/terremotospr/database/repositories/administrativeRepositories/ManagesRepository.java
+++ src/main/java/com/terremotospr/database/repositories/administrativeRepositories/ManagesRepository.java
@@ -1,8 +1,13 @@
 package com.terremotospr.database.repositories.administrativeRepositories;
 
+import com.terremotospr.beans.administrativeBeans.AccountStatus;
 import com.terremotospr.database.entities.administrativeEntities.Manages;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * @author Kiara Rodriguez Rojas
@@ -10,4 +15,20 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface ManagesRepository extends CrudRepository<Manages, Long> {
+    @Query(value = "select * from manages ma inner join user u on ma.user_id = u.id " +
+            "inner join admin a on ma.admin_id = a.id", nativeQuery = true)
+    List<Manages> findAll();
+
+    @Query(value = "select * from manages ma inner join user u on ma.user_id = u.id " +
+            "inner join admin a on ma.admin_id = a.id where ma.user_id = :userId", nativeQuery = true)
+    List<Manages> findByUserId(@Param("userId") Integer userId);
+
+    @Query(value = "select * from manages ma inner join user u on ma.user_id = u.id " +
+            "inner join admin a on ma.admin_id = a.id where ma.admin_id = :adminId", nativeQuery = true)
+    List<Manages> findByAdminId(@Param("adminId") Integer adminId);
+
+    @Query(value = "select * from manages ma inner join user u on ma.user_id = u.id " +
+            "inner join admin a on ma.admin_id = a.id where ma.status = :status", nativeQuery = true)
+    List<Manages> findByAccountStatus(@Param("adminId") AccountStatus status);
+
 }
