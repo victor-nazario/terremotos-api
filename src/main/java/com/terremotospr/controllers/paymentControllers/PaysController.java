564b21b30dcbd7995966641e92b9f76c0e9dd73d
Kiara Rodríguez Rojas <kiara.rodriguez18@upr.edu>
2020-05-04 05:29:37 -0400

    Added Pays queries: fetchAll and findBy attributes

--- src/main/java/com/terremotospr/controllers/paymentControllers/PaysController.java
+++ src/main/java/com/terremotospr/controllers/paymentControllers/PaysController.java
@@ -1,14 +1,10 @@
 package com.terremotospr.controllers.paymentControllers;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.terremotospr.beans.paymentBeans.PaysBean;
 import com.terremotospr.services.paymentServices.PaysService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
 import java.util.List;
 
 /**
@@ -26,16 +22,14 @@ public class PaysController {
         return paysService.addPays(bean);
     }
 
-//    @GetMapping(value = "/fetch")
-//    public List<PaysBean> fetchAll(){
-//        return paysService.fetchAllPays();
-//    }
-
     @GetMapping(value = "/fetch")
-    public Object fetchAll() throws IOException {
-        //To obtain the path, in IDEA right click and when the dialog shows up, select copy path -> path from source root
-        Resource resource = new ClassPathResource("responses/paysResponseJSON.json");
-        ObjectMapper mapper = new ObjectMapper();
-        return mapper.readValue(resource.getInputStream(), Object.class);
+    public List<PaysBean> fetchAll(){
+        return paysService.fetchAllPays();
     }
+
+    @GetMapping(value = "/payment/{id}")
+    public List<PaysBean> findByPaymentId(@PathVariable Integer id) { return paysService.findByPaymentId(id); }
+
+    @GetMapping(value = "/order/{id}")
+    public List<PaysBean> findByPlacedOrderId(@PathVariable Integer id) { return paysService.findByPlacedOrderId(id); }
 }
