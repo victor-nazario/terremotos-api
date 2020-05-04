34d442420cc5d556e8169d1c86bbc8232207e9d8
Kiara Rodríguez Rojas <kiara.rodriguez18@upr.edu>
2020-05-04 03:57:55 -0400

    Added Consumer queries: fetchAll and findBy attributes

--- src/main/java/com/terremotospr/services/administrativeServices/ConsumerService.java
+++ src/main/java/com/terremotospr/services/administrativeServices/ConsumerService.java
@@ -1,6 +1,7 @@
 package com.terremotospr.services.administrativeServices;
 
 import com.terremotospr.beans.administrativeBeans.ConsumerBean;
+import com.terremotospr.beans.administrativeBeans.ConsumerType;
 import com.terremotospr.database.entities.administrativeEntities.Consumer;
 import com.terremotospr.database.repositories.administrativeRepositories.ConsumerRepository;
 import org.springframework.beans.BeanUtils;
@@ -46,5 +47,42 @@ public class ConsumerService {
                 .collect(Collectors.toList());
 
         return consumers;
+    }
+
+    public Consumer findConsumerById(Integer id){
+        return consumerRepository.findById(id).get();
+    }
+
+    public List<ConsumerBean> findByCity(String city){
+        List<ConsumerBean> consumer;
+        Iterable<Consumer> iter = consumerRepository.findByCity(city);
+
+        consumer = StreamSupport.stream(iter.spliterator(), false)
+                .map(this::copyProperties)
+                .collect(Collectors.toList());
+
+        return consumer;
+    }
+
+    public List<ConsumerBean> findByRegion(String region){
+        List<ConsumerBean> consumer;
+        Iterable<Consumer> iter = consumerRepository.findByRegion(region);
+
+        consumer = StreamSupport.stream(iter.spliterator(), false)
+                .map(this::copyProperties)
+                .collect(Collectors.toList());
+
+        return consumer;
+    }
+
+    public List<ConsumerBean> findByType(ConsumerType type){
+        List<ConsumerBean> consumer;
+        Iterable<Consumer> iter = consumerRepository.findByType(type);
+
+        consumer = StreamSupport.stream(iter.spliterator(), false)
+                .map(this::copyProperties)
+                .collect(Collectors.toList());
+
+        return consumer;
     }
 }
