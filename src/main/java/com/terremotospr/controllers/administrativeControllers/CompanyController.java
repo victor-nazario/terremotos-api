af69988921b162d284f7af817a65be9c0b570a8e
Kiara Rodríguez Rojas <kiara.rodriguez18@upr.edu>
2020-05-04 04:39:59 -0400

    Added Company queries: fetchAll and findBy attributes

--- src/main/java/com/terremotospr/controllers/administrativeControllers/CompanyController.java
+++ src/main/java/com/terremotospr/controllers/administrativeControllers/CompanyController.java
@@ -1,14 +1,12 @@
 package com.terremotospr.controllers.administrativeControllers;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.terremotospr.beans.administrativeBeans.CompanyBean;
+import com.terremotospr.database.entities.administrativeEntities.Company;
 import com.terremotospr.services.administrativeServices.CompanyService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
+import java.util.List;
 
 /**
  * @author Kiara Rodriguez Rojas
@@ -27,16 +25,22 @@ public class CompanyController {
         return companyService.addCompany(bean);
     }
 
-//    @GetMapping(value = "/fetch")
-//    public List<CompanyBean> fetchAll(){
-//        return companyService.fetchAllCompany();
-//    }
-//
     @GetMapping(value = "/fetch")
-    public Object fetchAll() throws IOException {
-        //To obtain the path, in IDEA rightclick and when the dialog shows up, select copy path -> path from source root
-        Resource resource = new ClassPathResource("responses/companyResponseJSON.json");
-        ObjectMapper mapper = new ObjectMapper();
-        return mapper.readValue(resource.getInputStream(), Object.class);
+    public List<CompanyBean> fetchAll(){ return companyService.fetchAllCompany(); }
+
+    @GetMapping(value = "/{id}")
+    public Company findCompanyById(@PathVariable int id) {
+        return companyService.findCompanyById(id);
     }
+
+    @GetMapping(value = "/name/{name}")
+    public List<CompanyBean> findCompanyByName(@PathVariable String name) {
+        return companyService.findByCompanyName(name);
+    }
+
+    @GetMapping(value = "/location/{location}")
+    public List<CompanyBean> findCompanyByLocation(@PathVariable String location) {
+        return companyService.findByCompanyLocation(location);
+    }
+
 }
--- src/main/java/com/terremotospr/database/repositories/administrativeRepositories/CompanyRepository.java
+++ src/main/java/com/terremotospr/database/repositories/administrativeRepositories/CompanyRepository.java
@@ -1,8 +1,13 @@
 package com.terremotospr.database.repositories.administrativeRepositories;
 
 import com.terremotospr.database.entities.administrativeEntities.Company;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 /**
  * @author Kiara Rodriguez Rojas
@@ -11,4 +16,19 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CompanyRepository extends CrudRepository<Company,Long> {
+
+    @Query(value = "SELECT * FROM company co", nativeQuery = true)
+    List<Company> findAll();
+
+    @Query(value="SELECT * FROM company co WHERE co.id = :id", nativeQuery = true)
+    Optional<Company> findById(@Param("id") Integer id);
+
+    @Query(value = "SELECT * FROM company co WHERE co.cName = :cName"
+            , nativeQuery = true)
+    List<Company> findByCompanyName(@Param("name") String name);
+
+    @Query(value = "SELECT * FROM company co WHERE u.cLocation = :cLocation"
+            , nativeQuery = true)
+    List<Company> findByCompanyLocation(@Param("location") String location);
+
 }
