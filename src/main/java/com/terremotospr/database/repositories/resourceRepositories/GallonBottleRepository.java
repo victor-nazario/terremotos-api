a4e3499163ec7ad6ec4cdf2ec131a9ded0e29ef1
Victor Nazario <victor.nazario@upr.edu>
2020-05-04 23:11:59 -0400

    Gallon done

--- src/main/java/com/terremotospr/database/repositories/resourceRepositories/GallonBottleRepository.java
+++ src/main/java/com/terremotospr/database/repositories/resourceRepositories/GallonBottleRepository.java
@@ -1,8 +1,13 @@
 package com.terremotospr.database.repositories.resourceRepositories;
 
 import com.terremotospr.database.entities.resourceEntities.GallonBottle;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 /**
  * Created on  -
@@ -11,4 +16,30 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface GallonBottleRepository extends CrudRepository<GallonBottle, Long> {
+
+    @Query(value = "select * from gallon_bottle" +
+            " b inner join base_resource br on b.id = br.id where br.price <= :price ;"
+            , nativeQuery = true)
+    List<GallonBottle> findByPriceUnder(@Param("price") Double price);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    Optional<GallonBottle> findById(Long id);
+
 }
