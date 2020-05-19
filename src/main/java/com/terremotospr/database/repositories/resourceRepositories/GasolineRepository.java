ed3e5c5f2d99440a4b095667d67ca348d1b8284f
Kiara Rodríguez Rojas <kiara.rodriguez18@upr.edu>
2020-05-19 01:52:04 -0400

    Fixed gasoline, fuel and base resource join

--- src/main/java/com/terremotospr/database/repositories/resourceRepositories/GasolineRepository.java
+++ src/main/java/com/terremotospr/database/repositories/resourceRepositories/GasolineRepository.java
@@ -15,42 +15,52 @@ import java.util.List;
  */
 @Repository
 public interface GasolineRepository extends CrudRepository<Gasoline, Long> {
-    @Query(value = "select * from gasoline g inner join base_resource br on g.id = br.id", nativeQuery = true)
+    @Query(value = "select * from gasoline g inner join base_resource br inner join fuel f " +
+            "where g.id = br.id and g.id = f.id", nativeQuery = true)
     List<Gasoline> findAll();
 
-    @Query(value = "select * from gasoline g inner join base_resource br on g.id = br.id where g.id = :id"
+    @Query(value = "select * from gasoline g inner join base_resource br inner join fuel f " +
+            "where g.id = br.id and g.id = f.id and g.id = :id"
             , nativeQuery = true)
     Gasoline findById(@Param("id") Integer id);
 
-    @Query(value = "select * from gasoline g inner join base_resource br on g.id = br.id where br.price <= :price"
+    @Query(value = "select * from gasoline g inner join base_resource br inner join fuel f " +
+            "where g.id = br.id and g.id = f.id and br.price <= :price"
             , nativeQuery = true)
     List<Gasoline> findByPriceUnder(@Param("price") Double price);
 
-    @Query(value = "select * from gasoline g inner join base_resource br on g.id = br.id where br.price >= :price"
+    @Query(value = "select * from gasoline g inner join base_resource br inner join fuel f " +
+            "where g.id = br.id and g.id = f.id  and br.price >= :price"
             , nativeQuery = true)
     List<Gasoline> findByPriceOver(@Param("price") Double price);
 
-    @Query(value = "select * from gasoline g inner join base_resource br on g.id = br.id where br.brand = :brand"
+    @Query(value = "select * from gasoline g inner join base_resource br inner join fuel f " +
+            "where g.id = br.id and g.id = f.id and br.brand = :brand"
             , nativeQuery = true)
     List<Gasoline> findByBrand(@Param("brand") String brand);
 
-    @Query(value = "select * from gasoline g inner join base_resource br on g.id = br.id where br.name = :name"
+    @Query(value = "select * from gasoline g inner join base_resource br inner join fuel f " +
+            "where g.id = br.id and g.id = f.id and br.name = :name"
             , nativeQuery = true)
     List<Gasoline> findByName(@Param("name") String name);
 
-    @Query(value = "select * from gasoline g inner join base_resource br on g.id = br.id where br.available = true"
+    @Query(value = "select * from gasoline g inner join base_resource br inner join fuel f " +
+            "where g.id = br.id and g.id = f.id and br.available = true"
             , nativeQuery = true)
     List<Gasoline> findAvailable();
 
-    @Query(value = "select * from gasoline g inner join base_resource br on g.id = br.id where g.size = :size"
+    @Query(value = "select * from gasoline g inner join base_resource br inner join fuel f " +
+            "where g.id = br.id and g.id = f.id and  g.size = :size"
             , nativeQuery = true)
     List<Gasoline> findBySize(@Param("size") Double size);
 
-    @Query(value = "select * from gasoline g inner join base_resource br on g.id = br.id where g.octane = :octane"
+    @Query(value = "select * from gasoline g inner join base_resource br inner join fuel f " +
+            "where g.id = br.id and g.id = f.id and g.octane = :octane"
             , nativeQuery = true)
     List<Gasoline> findByOctane(@Param("octane") Double octane);
 
-    @Query(value = "select * from gasoline g inner join base_resource br on g.id = br.id where g.gasolineType = :gasolineType"
+    @Query(value = "select * from gasoline g inner join base_resource br inner join fuel f " +
+            "where g.id = br.id and g.id = f.id and g.gasoline_type = :gasolineType"
             , nativeQuery = true)
     List<Gasoline> findByType(@Param("gasolineType") String gasolineType);
 
