5d17775d95f308b145a72c2f3b5d35d88851f981
Kiara Rodríguez Rojas <kiara.rodriguez18@upr.edu>
2020-05-20 02:14:46 -0400

    Added final price for each resource in Belongs

--- src/main/java/com/terremotospr/services/administrativeServices/BelongsService.java
+++ src/main/java/com/terremotospr/services/administrativeServices/BelongsService.java
@@ -1,10 +1,10 @@
 package com.terremotospr.services.administrativeServices;
 
 import com.terremotospr.beans.administrativeBeans.BelongsBean;
-import com.terremotospr.beans.administrativeBeans.SuppliesBean;
 import com.terremotospr.database.entities.administrativeEntities.Belongs;
-import com.terremotospr.database.entities.administrativeEntities.Supplies;
+import com.terremotospr.database.entities.resourceEntities.BaseResource;
 import com.terremotospr.database.repositories.administrativeRepositories.BelongsRepository;
+import com.terremotospr.database.repositories.resourceRepositories.BaseResourceRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,6 +18,9 @@ public class BelongsService {
     @Autowired
     BelongsRepository belongsRepository;
 
+    @Autowired
+    BaseResourceRepository baseResourceRepository;
+
     public Boolean addBelongs(BelongsBean bean){
         if(bean == null) return false;
 
@@ -25,6 +28,11 @@ public class BelongsService {
         BeanUtils.copyProperties(bean, entity, "resource", "placedOrder");
         entity.setOrderId(bean.getOrderId());
         entity.setResourceId(bean.getResourceId());
+
+        BaseResource resource = baseResourceRepository.findById(bean.getResourceId()).get();
+        entity.setFinalPrice(resource.getPrice() * bean.getQuantity());
+        entity.setQuantity(bean.getQuantity());
+
         belongsRepository.save(entity);
         return true;
     }
